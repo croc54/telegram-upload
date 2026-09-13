b71e1fa9340afe19c032b74835976851590f5121
croc54 <rurouni.kenshin271@gmail.com>
2026-09-14 00:08:11 +0530

    Modernize FilePath for pathlib [ChatGPT]

--- telegram_upload/caption_formatter.py
+++ telegram_upload/caption_formatter.py
@@ -300,11 +300,7 @@ class FilePath(FileMixin, Path):
     def __new__(cls, *args, **kwargs):
         if cls is FilePath:
             cls = WindowsFilePath if os.name == 'nt' else PosixFilePath
-        self = cls._from_parts(args)
-        if not self._flavour.is_supported:
-            raise NotImplementedError("cannot instantiate %r on your system"
-                                      % (cls.__name__,))
-        return self
+        return cls(*args, **kwargs)
 
 
 class WindowsFilePath(FileMixin, WindowsPath):
